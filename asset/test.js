c0403ef09b2643ca6b8e8ab33f81b83dd510082f
L DC <da.cunha.luc@gmail.com>
2020-09-30 16:44:53 +0200

    ajouts des fichiers au dépôt

---
+++ asset/test.js
@@ -0,0 +1,42 @@
+function retrouveIndex() {
+    let indices = [];
+    let tableauEntiers = [1, 4, 6, 9, 16, 18, 59, 20, 30, 1, 9];
+    let nombreTest = Number(document.getElementById("champInput").value);
+    let idx = tableauEntiers.indexOf(nombreTest);
+
+    if (idx != -1) {
+        while (idx != -1) {
+            indices.push(idx);
+            idx = tableauEntiers.indexOf(nombreTest, idx + 1);
+        }
+
+        if (indices.length >= 2) {
+            document.querySelector('#testUn').innerText = `Le nombre ${nombreTest} est en position ${indices} dans le tableau. Il est donc présent plusieurs fois dans le tableau ! (le programme retourne true)`
+            return true;
+        }
+        else { document.querySelector('#testUn').innerText = `Le nombre ${nombreTest} est en position ${indices} dans le tableau.` }
+    }
+    else { document.querySelector('#testUn').innerText = `le nombre n'est pas présent dans le tableau` }
+}
+
+function lowerUpper() {
+    let phrase = "Testing text parse return value";
+    let upperPhrase = phrase.toUpperCase();
+    let lowerPhrase = phrase.toLowerCase();
+
+    document.querySelector('#testDeuxUpper').innerText = `La phrase en majuscule : ${upperPhrase}`
+    document.querySelector('#testDeuxLower').innerText = `La phrase en minuscule : ${lowerPhrase}`
+}
+
+function comparaisonParam() {
+    let paramUn = document.getElementById("paramUn").value;
+    let paramDeux = document.getElementById("paramDeux").value;
+
+    if (paramUn === paramDeux && paramUn !== "" && paramDeux !== "") {
+        document.querySelector('#testDeuxComparaison').innerText = `1`;
+    } 
+    else if (paramUn === "" || paramDeux === "") {
+        document.querySelector('#testDeuxComparaison').innerText = `L'un ou les deux paramètres sont vide`;
+    }
+    else {document.querySelector('#testDeuxComparaison').innerText = `0`;}
+}
