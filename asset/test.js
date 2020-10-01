4f92ab28320678542053ca7406fc6b63bf120d1d
L DC <da.cunha.luc@gmail.com>
2020-10-01 17:36:21 +0200

    mise à jours algo

--- asset/test.js
+++ asset/test.js
@@ -1,6 +1,8 @@
+let tableauEntiers = [1, 4, 6, 9, 16, 18, 59, 20, 30, 1, 9];
+//let phrase = "Testing text parse return value";
+
 function retrouveIndex() {
     let indices = [];
-    let tableauEntiers = [1, 4, 6, 9, 16, 18, 59, 20, 30, 1, 9];
     let nombreTest = Number(document.getElementById("champInput").value);
     let idx = tableauEntiers.indexOf(nombreTest);
 
@@ -14,29 +16,40 @@ function retrouveIndex() {
             document.querySelector('#testUn').innerText = `Le nombre ${nombreTest} est en position ${indices} dans le tableau. Il est donc présent plusieurs fois dans le tableau ! (le programme retourne true)`
             return true;
         }
-        else { document.querySelector('#testUn').innerText = `Le nombre ${nombreTest} est en position ${indices} dans le tableau.` }
+        else { 
+            document.querySelector('#testUn').innerText = `Le nombre ${nombreTest} est en position ${indices} dans le tableau.` 
+            return true;
+        }
     }
-    else { document.querySelector('#testUn').innerText = `le nombre n'est pas présent dans le tableau` }
-}
-
-function lowerUpper() {
-    let phrase = "Testing text parse return value";
-    let upperPhrase = phrase.toUpperCase();
-    let lowerPhrase = phrase.toLowerCase();
-
-    document.querySelector('#testDeuxUpper').innerText = `La phrase en majuscule : ${upperPhrase}`
-    document.querySelector('#testDeuxLower').innerText = `La phrase en minuscule : ${lowerPhrase}`
+    else { 
+        document.querySelector('#testUn').innerText = `le nombre n'est pas présent dans le tableau`
+        return false;
+    }
 }
 
 function comparaisonParam() {
+
     let paramUn = document.getElementById("paramUn").value;
     let paramDeux = document.getElementById("paramDeux").value;
+    let phraseDynamique = document.getElementById("phrase").value;
+    let upperPhrase = phraseDynamique.toUpperCase();
+    let lowerPhrase = phraseDynamique.toLowerCase(); 
+
+    document.querySelector('#testDeuxUpper').innerText = `La phrase en majuscule : ${upperPhrase}`;
+    document.querySelector('#testDeuxLower').innerText = `La phrase en minuscule : ${lowerPhrase}`;
+
+    console.log(paramUn);
+    console.log(paramDeux);
 
     if (paramUn === paramDeux && paramUn !== "" && paramDeux !== "") {
         document.querySelector('#testDeuxComparaison').innerText = `1`;
+        return 1;
     } 
     else if (paramUn === "" || paramDeux === "") {
-        document.querySelector('#testDeuxComparaison').innerText = `L'un ou les deux paramètres sont vide`;
+        document.querySelector('#testDeuxComparaison').innerText = `L'un ou les deux paramètres sont vides`;
     }
-    else {document.querySelector('#testDeuxComparaison').innerText = `0`;}
+    else {
+        document.querySelector('#testDeuxComparaison').innerText = `0`;
+        return 0;
+    }
 }
